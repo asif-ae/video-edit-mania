6acd0b488d0dc9d0f2cbdc50408ce1708021bd1a
Asif Estiak <me.asif.ae@gmail.com>
2021-04-18 21:30:09 +0600

    updated

--- src/components/Panel/Customar/Review/Review.js
+++ src/components/Panel/Customar/Review/Review.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { UserContext } from '../../../../App';
 import PanelHeader from '../../Shared/PanelHeader/PanelHeader';
